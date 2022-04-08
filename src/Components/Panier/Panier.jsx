04203048e3061528a2cfe447ae1098f3db4e8679
deepwwwave <deepwwwave@gmail.com>
2022-04-09 00:03:41 +0200

    upsdate structure

---
+++ src/Components/Panier/Panier.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Panier extends Component {
+  render() {
+    return (
+      <div>Panier</div>
+    )
+  }
+}
---
+++ src/Container/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import image1 from "../img/image1.webp"
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+          <img src={image1} alt="" />
+      </div>
+    )
+  }
+}
